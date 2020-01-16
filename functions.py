da8ecc628f6815730e48cee447ff9757d20c3448
Parasara IB <59839092+ParasaraIB@users.noreply.github.com>
2020-01-16 12:23:36 +0700

    create functions.py

---
+++ functions.py
@@ -0,0 +1,32 @@
+# Functions
+
+# Create variables x and y
+x = [1.0, 2.0, 3.0, 4.0]
+y = True
+# Print out type of x
+print(type(x))
+# Print out length of x
+print(len(x))
+# Convert y to an integer: y2
+y2 = int(y)
+
+# Help!
+
+# Getting the explanation of complex() function
+help(complex)
+
+# Multiple arguments
+
+# Create lists data1 and data2
+data1 = [9.75, 7.50, 10.00]
+data2 = [6.75, 5.25]
+# Paste together data1 and data2: comb
+comb = data1 + data2
+# Sort comb in ascending order: comb_sorted
+comb_sorted = sorted(comb, reverse = False)
+# Sort comb in descending order: comb_sorted2
+comb_sorted2 = sorted(comb, reverse = True)
+
+# Print out sorted comb
+print(comb_sorted)
+print(comb_sorted2)
