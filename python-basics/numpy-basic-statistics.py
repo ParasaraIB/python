036c9b4aa5a374e374866a32cf3c0feaf5f2c49c
Parasara IB <59839092+ParasaraIB@users.noreply.github.com>
2020-01-27 18:58:56 +0700

    update numpy-basic-statistics.py

--- python-basics/numpy-basic-statistics.py
+++ python-basics/numpy-basic-statistics.py
@@ -1,19 +1,25 @@
-# Numpy: basic statistics
+# Average versus median (1)
 
+# import file directory
+import os
+# get information about current directory
+print(os.getcwd())
+# open directory in which the data is stored
+os.chdir(r'D:\Data Science\Data Camp\Python Basic')
+# import pandas
+import pandas as pd
 # this example is using soccer player's heights, positions, weights (list type of data)
-heights = [191, 184, 185, 180, 181, 187, 170, 179, 183, 186, 185, 170]
-positions = ['GK', 'D', 'M', 'A', 'GK', 'D', 'M', 'A', 'GK', 'D', 'M', 'A',]
-weights = [90, 94, 82, 78, 85, 92, 63, 69, 75, 81, 88, 59]
+positions = pd.read_csv('fifa.csv', usecols = [3])
+heights = pd.read_csv('fifa.csv', usecols = [4])
 # import numpy
 import numpy as np
 # convert positons and heights to numpy arrays
-np_heights = np.array(heights)
 np_positions = np.array(positions)
-np_weights = np.array(weights)
+np_heights = np.array(heights)
 # heights of the goalkeepers
-gk_heights = np_heights[np_positions == 'GK']
+gk_heights = np_heights[np_positions == ' GK']
 # heights of the other players
-other_heights = np_heights[np_positions != 'GK']
+other_heights = np_heights[np_positions != ' GK']
 # print out the median height of GK ver. 1
 med_gk = np.median(gk_heights)
 print('Median height of GK: ' + str(med_gk))
@@ -24,21 +30,21 @@ print('Median height of other players: ' + str(np.median(other_heights)))
 # print out the standard deviation of the players' height
 print('Standard deviation of GK: ' + str(np.std(gk_heights)))
 print('Standard deviation of other players: ' + str(np.std(other_heights)))
-# print out the correlation between heights and weights
-print('Correlation: ' + str(np.corrcoef((np_heights), np_weights)))
+
+# Average versus median (2)
 
 # suppose the data is in the form of lists of list (subset)
-soccer = [[heights], [positions], [weights]]
+soccer = [np_positions, np_heights]
 # convert soccer to numpy array
 np_soccer = np.array(soccer)
+print(np_soccer)
 # convert the data to numpy arrays and float type since the types are conflicting
-np_heights2 = np.array(np_soccer[0,:]).astype(np.float)
-np_positions2 = np.array(np_soccer[1,:]).astype(np.str)
-np_weights2 = np.array(np_soccer[2,:]).astype(np.float)
+np_heights2 = np.array(np_soccer[1,:]).astype(np.float)
+np_positions2 = np.array(np_soccer[0,:]).astype(np.str)
 # heights of the goalkeepers
-gk_heights2 = np_heights2[np_positions2 == 'GK']
+gk_heights2 = np_heights2[np_positions2 == ' GK']
 # heights of the other players
-other_heights2 = np_heights2[np_positions2 != 'GK']
+other_heights2 = np_heights2[np_positions2 != ' GK']
 # print out the median height of GK
 print('Median 2 height of GK: ' + str(np.median(gk_heights2)))
 # print out the median height of other players
@@ -46,6 +52,3 @@ print('Median 2 height of other players: ' + str(np.median(other_heights2)))
 # print out the standard deviation of the players' height
 print('Standard deviation 2 of GK: ' + str(np.std(gk_heights2)))
 print('Standard deviation 2 of other players: ' + str(np.std(other_heights2)))
-# print out the correlation between heights and weights
-corr = np.corrcoef(np_heights2, np_weights2)
-print('Correlation 2: ' + str(corr))
