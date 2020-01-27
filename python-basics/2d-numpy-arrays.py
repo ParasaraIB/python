42ba7368556894d7d6ab22ca6607d3bda0733988
Parasara IB <59839092+ParasaraIB@users.noreply.github.com>
2020-01-27 18:05:20 +0700

    update 2d-numpy-arrays.py

--- python-basics/2d-numpy-arrays.py
+++ python-basics/2d-numpy-arrays.py
@@ -8,35 +8,52 @@ import numpy as np
 np_football = np.array(football)
 # print out the type of np_football
 print(type(np_football))
-# print out the shape of np_football
-print(np_football.shape)
+
+# Baseball data in 2D form
+
+# import file directory
+import os
+# get information about current directory
+print(os.getcwd())
+# open directory in which the data is stored
+os.chdir(r'D:\Data Science\Data Camp\Python Basic')
+# import pandas
+import pandas as pd
+# import baseball data
+baseball = pd.read_csv('baseball.csv', usecols = ['Height', 'Weight'])
+# create np_baseball
+np_baseball = np.array(baseball)
+# print out the shape of np_baseball
+print(np_baseball.shape)
 
 # Subsetting 2D numpy arrays
 
 # print out the 4th row of football (regular list of lists)
-print(football[3][0], football[3][1])
+print(np_baseball[3][0], np_baseball[3][1])
 # print out the 4th row of np_football using numpy
-print(np_football[3, :])
+print(np_baseball[3, :])
 # select the entire second column of np_football
-np_weight_lb = np_football[:, 0]
+np_weight_lb = np_baseball[:, 0]
 # print the result
 print(np_weight_lb)
 # print out the height of 3rd player
-print(np_football[2, 1])
+print(np_baseball[2, 1])
 
 # 2D arithmetic (units conversion)
 
+# import baseball data
+baseball = pd.read_csv('baseball.csv', usecols = ['Height', 'Weight', 'Age'])
+# create np_baseball
+np_baseball = np.array(baseball)
 # suppose the team's weight and height have changed
-update_football = [[-11.16, 1.23], [16.09, 1.02], [5.08, 1.15], [4.23, 1.09]]
-# create 2D numpy array from update_football
-np_update_football = np.array(update_football)
+update_baseball = pd.read_csv('updated.csv')
+# create 2D numpy array from update_baseball
+np_update_baseball = np.array(update_baseball)
 # update weight and height data
-updated = np_football + np_update_football
-# print out the addition of np_football and np_update_football
+updated = np_baseball + np_update_baseball
+# print out the addition of np_football and np_update_baseball
 print(updated)
-# the diffrence without using numpy array
-print(football + update_football)
 # convert the units
-conversion = np.array([0.453592, 0.0254])
+conversion = np.array([0.0254, 0.453592, 1])
 # print out the result
-print(updated * conversion)
+print(np_baseball * conversion)
