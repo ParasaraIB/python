4aa8d17fd89003754410715e183d5e94a6935cfc
Parasara IB <59839092+ParasaraIB@users.noreply.github.com>
2020-01-30 12:12:22 +0700

    update querying.py

--- importing-data-1/querying.py
+++ importing-data-1/querying.py
@@ -60,3 +60,52 @@ with engine.connect() as con :
 df.columns = rs.keys()
 # print head of DataFrame
 print(df.head())
+
+# === Pandas and SQL queries ===
+
+# import packages
+from sqlalchemy import create_engine
+import pandas as pd
+# create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+# execute query and store records in DataFrame: df
+df = pd.read_sql_query('SELECT * FROM Album', engine)
+# print head of DataFrame
+print(df.head())
+# open engine in context manager and store query result in df1
+with engine.connect() as con :
+    rs = con.execute('SELECT * FROM Album')
+    df1 = pd.DataFrame(rs.fetchall())
+    df1.columns = rs.keys()
+# confirm that both methods yield the same result
+print(df.equals(df1))
+
+# === Pandas for more complex querying ===
+
+# import packages
+from sqlalchemy import create_engine
+import pandas as pd
+# create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+# execute query and store records in DataFrame: df
+df = pd.read_sql_query('SELECT * FROM Employee WHERE EmployeeId >= 6 ORDER BY BirthDate', engine)
+# print head of DataFrame
+print(df.head())
+
+# === Relationships between tables: INNER JOIN ===
+
+# open engine in context manager
+# perform query and save the results to DataFrame: df
+with engine.connect() as con :
+    rs = con.execute('SELECT Title, Name FROM Album INNER JOIN Artist on Album.ArtistID = Artist.ArtistID')
+    df = pd.DataFrame(rs.fetchall())
+    df.columns = rs.keys()
+# print head of DataFrame df
+print(df.head())
+
+# === Filtering INNER JOIN ===
+
+# execute query and store records in DataFrame: df
+df = pd.read_sql_query('SELECT * FROM PlaylistTrack INNER JOIN Track on PlaylistTrack.TrackId = Track.TrackId WHERE Milliseconds < 250000', engine)
+# print head of DataFrame
+print(df.head())
